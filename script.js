89611567f5e796d4e5b3e68d39e4c9946e076738
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-02-02 12:26:30 -0600

    styling

--- script.js
+++ script.js
@@ -2,19 +2,24 @@
 var hoursOfDay = moment().startOf("day").add(9, "hour");
 for (var i=9; i<17; i++){
     var hours = hoursOfDay.add(1, "hour").format("hh:mm A");
+    
 };
 
+let currentHr = moment().hour();
+// let currentDate = moment().format("MMM Do YY"); 
+// currentDate.format();
+
+
 $(document).ready(function(){
+    for (var i=9; i<17; i++){
+        var hours = hoursOfDay.add(1, "hour").format("hh:mm A");
+    };
 
     $("#hour-by-hour").append(hours);
 
 });
-// // use this for sure
-// let currentHr = moment().hour();
-// let currentDate = moment().format("MMM Do YY"); 
 
-// currentDate.format();
+
 //INPUT FIELD AND SAVE BUTTON    
 //   to add a new appointment / item to scheduler
-
 //save button saves to LOCAL STORAGE
