641c50a28d08fc3b7fc21e466624754da30f69eb
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-02-02 10:43:30 -0600

    appending hours to page; only has one, working on it

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // harder; makes it dynamic -- eek
 var hoursOfDay = moment().startOf("day").add(9, "hour");
 for (var i=9; i<17; i++){
-    console.log(momentObj.add(1, "hour").format("hh:mm A"))
-}
+    var hours = hoursOfDay.add(1, "hour").format("hh:mm A");
+};
 
-$(hoursOfDay().append);
+$(document).ready(function(){
+
+    $("#hour-by-hour").append(hours);
+
+});
 // // use this for sure
 // let currentHr = moment().hour();
 // let currentDate = moment().format("MMM Do YY"); 
 
-currentDate.format();
+// currentDate.format();
 //INPUT FIELD AND SAVE BUTTON    
 //   to add a new appointment / item to scheduler
 
